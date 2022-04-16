53b887ab3cf9fb72aa5c89bbdfdb942ab9efc9c4
GStudent1 <1035193656@qq.com>
2022-04-16 19:46:34 +0800

    commit -m fix confict

--- run_commandline.bash
+++ run_commandline.bash
@@ -1,4 +1,6 @@
 ### -k 模糊匹配
 ### -m 运行mark自定义标记的case
-pytest testcase/study_skip/test_skip_first.py
+
+pytest testcase/study_mark/test_mark_first.py
 allure generate allure_report/xml -o allure_report/html --clean
+
